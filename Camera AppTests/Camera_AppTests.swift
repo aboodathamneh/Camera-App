fef7d3a363d8211676ce9858a10f86f917181f6c
Aboodslamehalathamneh <abood199944@gmail.com>
2018-11-13 10:28:22 +0200

    Initial Commit

---
+++ Camera AppTests/Camera_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Camera_AppTests.swift
+//  Camera AppTests
+//
+//  Created by Abdalrahman athamneh on 11/13/18.
+//  Copyright © 2018 Abdalrahman athamneh. All rights reserved.
+//
+
+import XCTest
+@testable import Camera_App
+
+class Camera_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
